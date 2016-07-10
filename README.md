eb0c39e4988c2c3259190a36423ec6c9ef4559f2
joextodd <joextodd@gmail.com>
2016-07-10 21:06:03 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# aws-serverless-skeleton
+## AWS Serverless Skeleton
+
 A skeleton project to easily utilise Amazon Web Service's serverless architecture with API Gateway and Lambda.
+Lambda functions are tested and deployed using Apex. API Gateway structure is defined with a Swagger file and
+deployed with aws-apigateway-importer.
+
+# Requirements
+
+- [Node.js](https://nodejs.org)
+- [Apex](http://apex.run)
+- [AWS API Gateway Importer](https://github.com/awslabs/aws-apigateway-importer)
---
+++ functions/saveData/index.js
@@ -0,0 +1,25 @@
+/*
+ * Example Lambda function to save data to DynamoDB.
+ */
+const aws = require('aws-sdk');
+const dynamo = new aws.DynamoDB.DocumentClient({ region: 'eu-west-1' });
+
+
+exports.handle = function handler(event, context) {
+  if (!event.data) {
+    context.fail('Bad Request: Missing data parameter.');
+    return;
+  }
+  var params = {
+    TableName : 'users',
+    Item: {
+       data: event.data,
+  };
+  dynamo.put(params, (err, data) => {
+    if (err) {
+      context.fail('Internal Error: Failed to update database.');
+      return;
+    }
+    context.succeed(data);
+  });
+};
